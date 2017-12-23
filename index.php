e8cdc0486dd23afdf146a9035b3b03550d8a57e2
Sanele13 <aynstyn@gmail.com>
2017-12-23 20:20:18 +0200

    I added a link to create account.

--- index.php
+++ index.php
@@ -8,13 +8,15 @@
 		<?php include("header.php"); ?>
 		<?php //include("menu.php"); ?>
 		<div class = "login">
-			<form action="login.php" method = "POST">
+			<form action="login/" method = "POST">
 				<input name = "username" type = "text" placeholder = "Username/email" class="login-input" /><br/><br/>
 				<input name = "password" type = "password" placeholder = "Password" class="login-input" /><br/><br/><br/>
 				<input id = "login-button" name = "submit" type = "submit" value="Log in">
-			</form>
+			</form><br/>
+			
 		</div>
-		<p><a href = ""><span style = "text-decoration: underline;">Create Account</span></a></p>
+		<br/>
+		<p style = "text-align: center;"><a href = "create/"><span style = "text-decoration: underline;text-align: center;">Create Account</span></a></p>
 		
 	</body>
 </html>
