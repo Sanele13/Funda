822421ec6ba0f2e4ea5d4e44c879a96223b10d7e
Sanele13 <aynstyn@gmail.com>
2017-12-28 15:52:41 +0200

    I can now add users to database.

--- create/index.php
+++ create/index.php
@@ -12,6 +12,8 @@
 			<input name = "mid-name" type = "text" placeholder = "Middle Name" class="login-input" /><br/><br/>
 			<input name = "surname" type = "text" placeholder = "Surname" class="login-input" /><br/><br/>
 			<input name = "ID" type = "text" placeholder = "ID Number" class="login-input" /><br/><br/>
+			<input name = "cell_number" type = "text" placeholder = "Cell Number" class="login-input" /><br/><br/>
+			<input name = "email_address" type = "text" placeholder = "Email Address" class="login-input" /><br/><br/>
 			<input name = "password" type = "password" placeholder = "Password" class="login-input" /><br/><br/>
 			<input name = "conf-password" type = "password" placeholder = "Confirm Password" class="login-input" /><br/><br/><br/>
 			<input id = "login-button" name = "submit" type = "submit" value="Sign Up">
--- create/signup/index.php
+++ create/signup/index.php
@@ -23,6 +23,8 @@ $link = mysqli_connect("localhost","root","zukiswa");
 $db = mysqli_select_db($link, "mydb");
 //echo $db;
 //	$sql_query = mysqli_query($link, "create table funda_users (name TEXT, mid_name TEXT, surname TEXT, ID TEXT, cell_number TEXT, email_address TEXT)"); -- created db for users
+//$query = 
+$sql_query = mysqli_query($link, "insert into funda_users (name,mid_name,surname,ID,cell_number,email_address) values ('{$name}','{$middleName}','{$lastName}','{$IdentityNum}','{$cell_number}','{$email_address}')");
 //$result = mysqli_fetch_assoc($sql_query);
 //echo $result;
 /*while ($row = mysqli_fetch_row($sql_query)) {
