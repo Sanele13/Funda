833ebac117ea9db9a534301d719a31ac4a52e376
Sanele13 <aynstyn@gmail.com>
2017-12-28 14:56:08 +0200

    Table created
    I have created a table for users in my database.

--- create/signup/index.php
+++ create/signup/index.php
@@ -18,14 +18,13 @@ $confPassword = $_POST["conf-password"];
 //add to database
 //1. connect to database
 $link = mysqli_connect("localhost","root","zukiswa");
-//2. select database
 $db = mysqli_select_db($link, "mydb");
-
-$sql_query = mysqli_query($link, "create table funda (TEXT name, TEXT mid_name, TEXT surname, VARCHAR(13) ID_num, TEXT email_address, VARCHAR(10)cell_number);");
-$result = mysqli_fetch_assoc($sql_query);
+//echo $db;
+//	$sql_query = mysqli_query($link, "create table funda_users (name TEXT, mid_name TEXT, surname TEXT, ID TEXT, cell_number TEXT, email_address TEXT)"); -- created db for users
+//$result = mysqli_fetch_assoc($sql_query);
 //echo $result;
 /*while ($row = mysqli_fetch_row($sql_query)) {
-printf ("%s (%s)\n", $row[0], $row[1]);
+printf ($row[0]);
 }*/
 
 ?>
