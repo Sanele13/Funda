bc325b9a13f1480a44321735f443a23cf5377320
Sanele13 <aynstyn@gmail.com>
2018-06-23 13:28:37 +0200

    moved app.php to app folder, and created a classroom view

---
+++ app/classroom.php
@@ -0,0 +1,24 @@
+<?php include '../switch.php'; ?>
+<html>
+	<head>
+		<title>Funda-Making a difference through education</title>
+		<link rel = 'stylesheet' type = 'text/css' href = '<?php echo $host_url; ?>/css/fundastyle.css'>
+		<link rel = 'stylesheet' type = 'text/css' href = '<?php echo $host_url; ?>/css/classroom.css'>
+		<script src="<?php echo $host_url; ?>/js/vue.js"></script>
+	</head>
+	<body>
+		<div class="menu">
+			<div class="bar-container">
+				<div class="bar"></div>
+				<div class="bar"></div>
+				<div class="bar"></div>
+			</div>
+		</div>
+		<div class = "outer-container">
+			<div class="inner-container">
+			</div>
+			
+		</div>
+		
+	</body>
+</html>
