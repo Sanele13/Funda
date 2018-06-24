1f4c86d6bc2c45cf588f9d863c0f05120455ad7b
Sanele13 <aynstyn@gmail.com>
2018-06-24 09:10:21 +0200

    added a dropdown menu in the classroom

--- app/classroom.php
+++ app/classroom.php
@@ -7,13 +7,25 @@
 		<script src="<?php echo $host_url; ?>/js/vue.js"></script>
 	</head>
 	<body>
-		<div class="menu">
-			<div class="bar-container">
-				<div class="bar"></div>
-				<div class="bar"></div>
-				<div class="bar"></div>
+		<div class="menu-container">
+			<div class="menu-icon">
+				<div class="bar-container">
+					<div class="bar"></div>
+					<div class="bar"></div>
+					<div class="bar"></div>
+				</div>
 			</div>
+			<div class="menu">
+				<div class="menu-options">
+					<div class="menu-opt">Subjects</div>
+					<div class="menu-opt">Mock Tests & Exams</div>
+					<div class="menu-opt">Quizzes</div>
+					<div class="menu-opt">Competitions</div>
+					<div class="menu-opt">Discussions</div>
+				</div>
+			</div>	
 		</div>
+		
 		<div class = "outer-container">
 			<div class="inner-container">
 				<?php include 'app.php';?>
